3b6c260c63c8439860d892a2b4313f89dd607400
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-03-21 12:18:15 -0500

    move wrapper to app

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/PortfolioPage";
 import "./style/index.css";
+import Wrapper from "./components/Wrapper/wrapper";
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/reactpf" />
-          </Route>
-          <Route exact path="/reactpf" component={Home}></Route>
-          <Route exact path="/contact" component={Contact}></Route>
-          <Route exact path="/portfolio" component={Portfolio}></Route>
-        </Switch>
+        <Wrapper>
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/reactpf" />
+            </Route>
+            <Route exact path="/reactpf" component={Home}></Route>
+            <Route exact path="/contact" component={Contact}></Route>
+            <Route exact path="/portfolio" component={Portfolio}></Route>
+          </Switch>
+        </Wrapper>
         <Footer />
       </div>
     </Router>
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,21 +1,17 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav } from "react-bootstrap";
-import { Link } from 'react-router-dom'
 import "../../style/index.css";
 
 function NavbarTemplate() {
   return (
     <Navbar className="nvbg">
       <Nav className="mr-auto">
-        <Nav.Link to="/reactpf">About Me</Nav.Link>
-        <Nav.Link to="/contact">Contact</Nav.Link>
-        <Nav.Link to="/portfolio">Portfolio</Nav.Link>
+        <Nav.Link href="/reactpf">About Me</Nav.Link>
+        <Nav.Link href="/contact">Contact</Nav.Link>
+        <Nav.Link href="/portfolio">Portfolio</Nav.Link>
       </Nav>
     </Navbar>
-
-
-
   );
 }
 
